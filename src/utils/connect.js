e9ecd24c319b65e6365e2731742a72c299b59704
George-Laptop <glthomas526@gmail.com>
2023-04-21 12:44:06 -0400

    fixed connection uri

--- src/utils/connect.js
+++ src/utils/connect.js
@@ -5,7 +5,7 @@ dotenv.config();
 
 const mongoConnection = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI, {
+    await mongoose.connect(process.env.MONGODB_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
